85a6e3d393ee69e94b3c6d093298bcf247080de3
harold <wtakoutsing@gmail.com>
2016-03-28 11:05:10 +0100

    ajout des entités

---
+++ SYSOP/src/main/java/cm/mycompany/sysop/data/ClasseOuvrier.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cm.mycompany.sysop.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+/**
+ *
+ * @author harold
+ */
+@Entity
+public class ClasseOuvrier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
+}
