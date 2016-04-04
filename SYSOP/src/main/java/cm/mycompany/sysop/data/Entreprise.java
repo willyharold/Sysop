fef985697268b0e68aee52172359310d3703bc00
schangmokai <doumtsopmelifranck@yahoo.fr>
2016-04-04 13:00:06 +0100

    initialisation de la couche service

--- SYSOP/src/main/java/cm/mycompany/sysop/data/Entreprise.java
+++ SYSOP/src/main/java/cm/mycompany/sysop/data/Entreprise.java
@@ -11,11 +11,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-import org.hibernate.annotations.ManyToAny;
 
 /**
  *
