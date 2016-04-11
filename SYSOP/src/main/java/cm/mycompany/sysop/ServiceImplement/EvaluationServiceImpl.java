95480c7377242c7d0ffaa5e858b27a9be7ecfcf5
schangmokai <doumtsopmelifranck@yahoo.fr>
2016-04-11 18:51:11 +0100

    configuration du Spring pas complet attendant vos services implements

--- SYSOP/src/main/java/cm/mycompany/sysop/ServiceImplement/EvaluationServiceImpl.java
+++ SYSOP/src/main/java/cm/mycompany/sysop/ServiceImplement/EvaluationServiceImpl.java
@@ -5,10 +5,73 @@
  */
 package cm.mycompany.sysop.ServiceImplement;
 
+import cm.mycompany.sysop.Dao.Interface.IEvaluationDao;
+import cm.mycompany.sysop.ServiceInterface.IEvaluationService;
+import cm.mycompany.sysop.data.Evaluation;
+import com.douwe.generic.dao.DataAccessException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author doumtsop
  */
-public class EvaluationServiceImpl {
+@Transactional
+public class EvaluationServiceImpl implements IEvaluationService{
+    
+    IEvaluationDao iEvaluationDao;
+
+    public IEvaluationDao getiEvaluationDao() {
+        return iEvaluationDao;
+    }
+
+    public void setiEvaluationDao(IEvaluationDao iEvaluationDao) {
+        this.iEvaluationDao = iEvaluationDao;
+    }
+    
+    
+    
+    public Evaluation createEvaluation(Evaluation evaluation) throws ServiceException {
+        try {
+            // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            return iEvaluationDao.create(evaluation);
+        } catch (DataAccessException ex) {
+            Logger.getLogger(EvaluationServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ServiceException("erreur de creation");
+        }
+    }
+
+    public Evaluation updateEvaluation(Evaluation evaluation) throws ServiceException {
+        try {
+            // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            return iEvaluationDao.update(evaluation);
+        } catch (DataAccessException ex) {
+            Logger.getLogger(EvaluationServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ServiceException("erreur de creation");
+        }
+    }
+
+    public Evaluation findEvaluationById(Long id) throws ServiceException {
+        try {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            return iEvaluationDao.findById(id);
+        } catch (DataAccessException ex) {
+            Logger.getLogger(EvaluationServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ServiceException("erreur de creation");
+        }
+    }
+
+    public List<Evaluation> findAllEvaluation() throws ServiceException {
+        try {
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            return iEvaluationDao.findAll();
+        } catch (DataAccessException ex) {
+            Logger.getLogger(EvaluationServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ServiceException("erreur de creation");
+        }
+    }
     
 }
--- SYSOP/src/main/java/cm/mycompany/sysop/ServiceInterface/IEvaluationService.java
+++ SYSOP/src/main/java/cm/mycompany/sysop/ServiceInterface/IEvaluationService.java
@@ -5,10 +5,19 @@
  */
 package cm.mycompany.sysop.ServiceInterface;
 
+import cm.mycompany.sysop.data.Evaluation;
+import java.util.List;
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author doumtsop
  */
+@Transactional
 public interface IEvaluationService {
-    
+    public Evaluation createEvaluation(Evaluation evaluation) throws ServiceException;
+    public Evaluation updateEvaluation(Evaluation evaluation) throws ServiceException;
+    public Evaluation findEvaluationById(Long id) throws ServiceException;
+    public List<Evaluation> findAllEvaluation() throws ServiceException;
 }
